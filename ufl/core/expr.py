cc337edd204b7eab1ebd34ea57d42cc7ac630db7
Min RK <benjaminrk@gmail.com>
2023-01-25 11:46:27 +0100

    set stacklevel=2 on DeprecationWarnings
    this applies the warning to the caller, so it's easier to find what code is using the deprecated API

--- ufl/core/expr.py
+++ ufl/core/expr.py
@@ -267,14 +267,18 @@ class Expr(object, metaclass=UFLType):
     def ufl_domains(self):
         "Return all domains this expression is defined on."
         warnings.warn("Expr.ufl_domains() is deprecated, please "
-                      "use extract_domains(expr) instead.", DeprecationWarning)
+                      "use extract_domains(expr) instead.",
+                      DeprecationWarning,
+                      stacklevel=2)
         from ufl.domain import extract_domains
         return extract_domains(self)
 
     def ufl_domain(self):
         "Return the single unique domain this expression is defined on, or throw an error."
         warnings.warn("Expr.ufl_domain() is deprecated, please "
-                      "use extract_unique_domain(expr) instead.", DeprecationWarning)
+                      "use extract_unique_domain(expr) instead.",
+                      DeprecationWarning,
+                      stacklevel=2)
         from ufl.domain import extract_unique_domain
         return extract_unique_domain(self)
 
